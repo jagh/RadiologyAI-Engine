7e3294805116cbdb70e1be4a8253a7ce9f24068e
JAGH <jagh1729@gmail.com>
2020-11-04 15:13:41 +0100

    Updated the bi-lung and lung lobes segmentation class

--- engine/segmentations.py
+++ engine/segmentations.py
@@ -7,7 +7,7 @@ from engine.third_party.lungmask import mask
 from engine.third_party.lungmask import resunet
 
 
-class LungLobesSegmentation:
+class LungSegmentations:
     """
     Lung lobes segmentation using a lungmask module
     """
@@ -16,11 +16,19 @@ class LungLobesSegmentation:
         print("++ Welcome to Lung Segmentation")
         pass
 
-    def file_segmentation(self, input_ct, batch_size=10):
+    def file_segmentation(self, input_ct, seg_method='lobes', batch_size=10):
         """
         CT lung lobes segmentation using UNet
         """
-        model = mask.get_model('unet','LTRCLobes')
+
+        if seg_method == 'bi-lung':
+            model = mask.get_model('unet', 'R231')
+            print('R231-method')
+        elif seg_method == 'lobes':
+            model = mask.get_model('unet', 'LTRCLobes')
+        else:
+            print('Segmentation method not found!')
+
         ct_segmentation = mask.apply(input_ct, model, batch_size=batch_size)
 
         ### Write segmentation
@@ -31,7 +39,8 @@ class LungLobesSegmentation:
         return result_out
 
 
-    def folder_segmentations(self, input_folder, output_folder, batch_size=10):
+
+    def folder_segmentations(self, input_folder, output_folder, seg_method='lobes', batch_size=10):
         """
         CT Lung lobes segmentation for all nii.gz files within the directory.
         """
@@ -40,10 +49,10 @@ class LungLobesSegmentation:
             # print("engine - input_path", input_path)
 
             ct_name = input_path.split(os.path.sep)[-1]
-            ct_dcm_format = str(ct_name.split('.nii.gz')[0] + "-lung_lobes.nii.gz")
+            ct_dcm_format = str(ct_name.split('.nii.gz')[0] + '-' + seg_method + '.nii.gz')
 
             input_ct = sitk.ReadImage(input_path)
-            result_out = self.file_segmentation(input_ct, batch_size)
+            result_out = self.file_segmentation(input_ct, seg_method, batch_size)
 
             Utils().mkdir(output_folder)
             sitk.WriteImage(result_out, str(output_folder+"/"+ct_dcm_format))
