43f26e1f19f1e7dcc849e0bfccc8b8733870529e
jagh <jagh1729@gmail.com>
2021-10-29 10:10:49 +0200

    Updated script to compose the lesion segmentation

--- lesion_segmentation.py
+++ lesion_segmentation.py
@@ -13,13 +13,14 @@ from engine.utils import Utils
 from engine.preprocessing import ImageProcessing
 
 
-def convert_images(dataframe, cts_folder, seg_folder, sandbox='sandbox', split='train'):
+def convert_axial_slices(dataframe, cts_folder, seg_folder, sandbox='sandbox', split='train'):
     """
     This function converts the dataset into a format that is compatible with the nnUNet framework.
     The function takes as input the metadata file, the folder containing the CTs, and
     the folder containing the manual segmentations.
     Finally, the function iterates over the metadata file and creates a Nifit file
     for each axial slice with shape (x, y, 1).
+
     Parameters
     ----------
         dataframe (str): The metadata file.
@@ -37,104 +38,43 @@ def convert_images(dataframe, cts_folder, seg_folder, sandbox='sandbox', split='
     ## Using separate folder for training and test
     if split == 'test':
         metadata = metadata_full.query('split == "test"')
-        axial_slices_folder = os.path.join(sandbox, str("imagesTs"))
-        axial_labels_folder = os.path.join(sandbox, str("labelsTs"))
+        axial_slices_folder = os.path.join(sandbox, str("All_axial_imagesTs"))
+        # axial_labels_folder = os.path.join(sandbox, str("axial_labelsTs"))
     else:
         metadata = metadata_full.query('split == "train"')
-        axial_slices_folder = os.path.join(sandbox, str("imagesTr"))
-        axial_labels_folder = os.path.join(sandbox, str("labelsTr"))
+        axial_slices_folder = os.path.join(sandbox, str("All_axial_imagesTr"))
+        # axial_labels_folder = os.path.join(sandbox, str("labelsTr"))
 
+    ## Remove axial slices rows and resert index
+    metadata = metadata.drop_duplicates(subset=['id_case'])
     metadata = metadata.reset_index(drop=True)
     print("++ Metadata {}: {}".format(split, metadata.shape))
 
     ## Create a directory for images and labels inside the Sandbox directory
     Utils().mkdir(axial_slices_folder)
-    Utils().mkdir(axial_labels_folder)
+    # Utils().mkdir(axial_labels_folder)
 
     ## Iterate between axial slices
     for row in range(metadata.shape[0]):
         ## Locating the CTs and labels
         ct_file_name = os.path.join(cts_folder, metadata['ct_file_name'][row])
-        lesion_file_name = os.path.join(seg_folder, metadata['lesion_file_name'][row])
+        print("+ ct_file_name: ", ct_file_name)
 
-        ## Fix the position of the slice and check for lesion
-        axial_index = metadata['slice_position'][row]-1
-        axial_with_lesion = metadata['slice_with_lesion'][row]
+        ## get the ct array
+        image = nib.load(ct_file_name)
+        image_array = image.get_fdata()
 
-        ct_nifti = ImageProcessing().extract_axial_slice_3D(ct_file_name, axial_index)
-        lesion_nifti = ImageProcessing().extract_axial_slice_3D(lesion_file_name, axial_index)
+        ## Loop into all axial slices by CT scan
+        for axial_index in range(image_array.shape[2]):
 
-        ## Set the file name for each axial slice and add the mode _0000 for SK
-        ct_slice_name = str(metadata['id_case'][row]) + '-' + str(axial_index) + '_0000.nii.gz'
-        lesion_slice_name = str(metadata['id_case'][row]) + '-' + str(axial_index) + '.nii.gz'
+            ct_nifti = ImageProcessing().extract_axial_slice_3D(ct_file_name, axial_index)
 
-        ## Write axial slices to a Nifti file for each axial slice
-        nib.save(ct_nifti, os.path.join(axial_slices_folder, ct_slice_name))
-        nib.save(lesion_nifti, os.path.join(axial_labels_folder, lesion_slice_name))
+            ## Set the file name for each axial slice and add the mode _0000 for SK
+            ct_slice_name = str(metadata['id_case'][row]) + '-' + str(axial_index) + '_0000.nii.gz'
 
+            ## Write axial slices to a Nifti file for each axial slice
+            nib.save(ct_nifti, os.path.join(axial_slices_folder, ct_slice_name))
 
-def generate_json(sandbox='sandbox'):
-    """
-    Generate a json file to feed the nnunet framework.
-    The json file contains the following information:
-    - name: name of the dataset
-    - description: description of the dataset
-    - tensorImageSize: 3D or 2D
-    - reference: reference of the dataset
-    - licence: license of the dataset
-    - release: release of the dataset
-    - modality: modalities of the dataset
-    - labels: labels of the dataset
-    - numTraining: number of training samples
-    - numTest: number of test samples
-    - training: list of training samples
-    - test: list of test samples
-    Parameters
-    ----------
-        sandbox (str): Path to the sandbox folder
-    Returns
-    -------
-        None
-    """
-
-    ## Dataset conversion params
-    imagesTr_dir            = os.path.join(sandbox, "imagesTr")
-    imagesTs_dir            = os.path.join(sandbox, "imagesTs")
-    modalities              = ["SK"]
-    labels                  = {0: 'foreground', 1: 'GGO', 2: 'CON', #3: 'ATE',
-                                                3: 'PLE', 4: 'BAN', 5: 'TBR'}
-    dataset_name            = "Task115_COVID-19",
-    license                 = "Hands on",
-    dataset_description     = "Axial slice multi-class lesion segmentation for covid-19 patients",
-    dataset_reference       = "Multiomics 2D slices",
-    dataset_release         = '0.1'
-
-    ## Get indetifier files
-    train_identifiers = Utils().get_file_identifiers(imagesTr_dir)
-    test_identifiers = Utils().get_file_identifiers(imagesTs_dir)
-
-    ## Construction of json to feed the nnunet framework
-    output_file = os.path.join(sandbox, "dataset.json")
-    json_dict   = {}
-    json_dict['name']               = dataset_name
-    json_dict['description']        = dataset_description
-    json_dict['tensorImageSize']    = "3D"
-    json_dict['reference']          = dataset_reference
-    json_dict['licence']            = license
-    json_dict['release']            = dataset_release
-    json_dict['modality']           = {str(i): modalities[i] for i in range(len(modalities))}
-    json_dict['labels']             = {str(i): labels[i] for i in labels.keys()}
-
-    json_dict['numTraining']        = len(train_identifiers)
-    json_dict['numTest']            = len(test_identifiers)
-    json_dict['training']           = [{'image': "./imagesTr/%s.nii.gz" % i,
-                                        "label": "./labelsTr/%s.nii.gz" % i} for i in train_identifiers]
-    json_dict['test']               = ["./imagesTs/%s.nii.gz" % i for i in test_identifiers]
-
-    ## Write output file in sandbox folder
-    if not os.path.exists(output_file):
-        os.path.join(sandbox, "dataset.json")
-    Utils().save_json(json_dict, os.path.join(output_file))
 
 
 
@@ -146,18 +86,16 @@ def run(args):
     :param seg_folder: Path to the folder containing the segmentations
     :param sandbox: Path to the sandbox where intermediate results are stored
     """
-    convert_images(args.dataframe, args.cts_folder, args.seg_folder, args.sandbox, "train")
-    convert_images(args.dataframe, args.cts_folder, args.seg_folder, args.sandbox, "test")
-    # generate_json(args.sandbox)
+    convert_axial_slices(args.dataframe, args.cts_folder, args.sandbox, "train")
+    convert_axial_slices(args.dataframe, args.cts_folder, args.sandbox, "test")
 
 
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--sandbox', default='/data/01_UB/00_Dev/sandbox/')
-    parser.add_argument('-d', '--dataframe', default='/data/01_UB/00_Dev/2_dataframe_axial_slices.csv')
-    parser.add_argument('-cts', '--cts_folder', default='/data/01_UB/00_Dev/01_Nifti-Data/')
-    parser.add_argument('-seg', '--seg_folder', default='/data/01_UB/00_Dev/sandbox/00-relabel_folder/')
+    parser.add_argument('-s', '--sandbox', default='/data/01_UB/nnUNet_Sandbox/nnUNet_raw_data_base/nnUNet_raw_data/Task115_COVIDSegChallenge')
+    parser.add_argument('-d', '--dataframe', default='/home/jagh/Documents/01_conferences_submitted/00_MIA-Explainable_2021/dataset/122_dataframe_axial_slices.csv')
+    parser.add_argument('-cts', '--cts_folder', default='/data/01_UB/Multiomics-Data/Clinical_Imaging/02_Step-3_122-CasesSegmented/01_Nifti-Data/')
 
     args = parser.parse_args()
     run(args)
